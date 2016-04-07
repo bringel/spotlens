714621fd50e94ce6445bd5ccc0d4a92ab2f8a892
Brad Ringel <brad.ringel@gmail.com>
2016-04-06 21:38:20 -0500

    Starting the admin page with some styling and buttons

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,9 @@ require('oauth2')
 class AdminController < ApplicationController
   def index
 
-    @oauth_handler = OAuth2::Client.new("7617e91899434659a4baad896112985f", "32581f8b329f40f5827db7c218625d49", :site => "https://instagram.com")
+    @instagram_oauth_handler = OAuth2::Client.new(ENV["INSTAGRAM_CLIENT_ID"], ENV["INSTAGRAM_CLIENT_SECRET"], :site => "https://instagram.com")
+
+    @twitter_oauth_handler = OAuth2::Client.new(ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], :site => "https://twitter.com")
   end
 
   def instagram_callback
