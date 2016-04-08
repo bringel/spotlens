bb9032818ac9017ecefe5fcdaaf88217d451640a
Brad Ringel <brad.ringel@gmail.com>
2016-04-07 20:03:01 -0500

    Instagram OAuth is working correctly, now for some persistence

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,29 @@
 require('oauth2')
-
+require('byebug')
 class AdminController < ApplicationController
   def index
+    instagram_oauth_handler = OAuth2::Client.new(ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET'], :site => "https://api.instagram.com")
+    @instagram_oauth_code_link = instagram_oauth_handler.auth_code.authorize_url(:redirect_uri => instagram_callback_url, :scope => "public_content")
 
-    @instagram_oauth_handler = OAuth2::Client.new(ENV["INSTAGRAM_CLIENT_ID"], ENV["INSTAGRAM_CLIENT_SECRET"], :site => "https://instagram.com")
-
-    @twitter_oauth_handler = OAuth2::Client.new(ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], :site => "https://twitter.com")
+    @twitter_oauth_handler = OAuth2::Client.new(ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET'], :site => "https://twitter.com")
+    @twitter_oauth_code_link = @twitter_oauth_handler.auth_code.authorize_url(:redirect_url => twitter_callback_url)
   end
 
   def instagram_callback
+    instagram_oauth_handler = OAuth2::Client.new(ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET'], :site => "https://api.instagram.com", :token_url => "/oauth/access_token")
+
+    @token = instagram_oauth_handler.auth_code.get_token(params[:code], :redirect_uri => "#{request.protocol}#{request.host}#{request.port_string}/admin/instagram_callback")
+    redirect_to action: "index"
+  end
+
+  def twitter_callback
+  end
+
+  def instagram_callback_url
+    "#{request.protocol}#{request.host_with_port}/admin/instagram_callback"
+  end
+
+  def twitter_callback_url
+    "#{request.protocol}#{request.host_with_port}/admin/twitter_callback"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'admin/instagram_callback' => 'admin#instagram_callback'
 
+  get 'admin/twitter_callback' => 'admin#twitter_callback'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
