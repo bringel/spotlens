1d2d971a0bb5c037e960b68ae7cc51ac1c64a1b3
Brad Ringel <brad.ringel@gmail.com>
2016-04-18 22:04:15 -0500

    add some photo fetching logic

--- lib/instagram_client.rb
+++ lib/instagram_client.rb
@@ -2,5 +2,17 @@ require('oauth2')
 require('httparty')
 
 class InstagramClient
+  def initialize(token)
+    @oauthToken = token
+    @baseURL = "https://api.instagram.com/v1"
+  end
 
+  def mostRecentPhotosForHashtag(hashtag)
+    tagURL = "#{@baseURL}/tags/#{hashtag}/media/recent?access_token=#{@oauthToken}"
+
+    response = HTTParty.get(tagURL)
+    return unless response.code >= 200 && response.code < 300
+
+
+  end
 end
