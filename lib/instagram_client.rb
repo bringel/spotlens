7066a3ac85a92237acbead6cdc81042370232ffc
Brad Ringel <brad.ringel@gmail.com>
2016-04-21 18:46:52 -0500

    Parse the response and save to the database

--- lib/instagram_client.rb
+++ lib/instagram_client.rb
@@ -13,6 +13,20 @@ class InstagramClient
     response = HTTParty.get(tagURL)
     return unless response.code >= 200 && response.code < 300
 
+    responseData = JSON.parse(response.body)
+    responseData["data"].each do |photo|
+      break unless photo["type"] == "image"
 
+      caption = photo["caption"]["text"]
+      likes = photo["likes"]["count"]
+      username = photo["user"]["username"]
+      profile_picture = photo["user"]["profile_picture"]
+      fullname = photo["user"]["full_name"]
+      postTime = photo["created_time"]
+      photoUrl = photo["images"]["standard_resolution"]["url"]
+      photoID = photo["id"]
+
+      InstagramPhoto.create({ :instagram_id => photoID, :photo_url => photoUrl, :instagram_username => username, :instagram_profile_photo => profile_picture, :instagram_fullname => fullname, :caption => caption, :likes => likes, :post_time => postTime })
+    end
   end
 end
