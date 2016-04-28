91f6265886b2bd8cec74f83632e8abf033871b51
Brad Ringel <brad.ringel@gmail.com>
2016-04-28 11:04:33 -0500

    return json for the next photo

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -4,6 +4,15 @@ class StreamController < ApplicationController
 
   def next_instagram_photo
     respond_to(:json)
+
+    if params[:current].empty?
+      nextPhoto = InstagramPhoto.first
+    else
+      id = params[:current].to_i + 1
+      nextPhoto = InstagramPhoto.find(id)
+    end
+
+    render({:json => nextPhoto})
   end
 
   def next_twitter_photo
