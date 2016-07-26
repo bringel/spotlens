1debf605186e325219bee067cdc5bae62c4ef136
Brad Ringel <brad.ringel@gmail.com>
2016-07-26 18:34:05 -0400

    Create new record for new messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,18 @@
 class TwilioController < ApplicationController
   def new_message
-    
+    body = JSON.decode(request.body)
+
+    @message = TwilioMessage.create({
+                                     :message_sid => body['MessageSid'],
+                                     :account_sid => body['AccountSid'],
+                                     :messaging_service_sid => body['MessagingServiceSid'],
+                                     :from => body['From'],
+                                     :to => body['To'],
+                                     :body => body['Body'],
+                                     :num_media => body['NumMedia'],
+                                     :media_content_type => body['MediaContentType'],
+                                     :media_url => body['MediaUrl']
+                                  })
+    Rails.logger.debug(@message.to_json)
   end
 end
