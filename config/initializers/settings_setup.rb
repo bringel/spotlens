068a7cf1fa9e67fd3bb62592e2e589e82da9c98e
Brad Ringel <brad.ringel@gmail.com>
2016-07-26 18:42:14 -0400

    Check if the application settings table exists before using it

--- config/initializers/settings_setup.rb
+++ config/initializers/settings_setup.rb
@@ -1,10 +1,11 @@
+if ActiveRecord::Base.connection.table_exists?('application_settings')
+  appSettings = ApplicationSetting.first
 
-appSettings = ApplicationSetting.first
+  if appSettings == nil
+    appSettings = ApplicationSetting.create({ :photo_fetch_timer => 60, :photo_switch_timer => 30, :hashtags => '[]' })
+  end
 
-if appSettings == nil
-  appSettings = ApplicationSetting.create({ :photo_fetch_timer => 60, :photo_switch_timer => 30, :hashtags => '[]' })
+  ENV['photo_fetch_timer'] = appSettings.photo_fetch_timer.to_s || '60'
+  ENV['photo_switch_timer'] = appSettings.photo_switch_timer.to_s || '30'
+  ENV['hashtags'] ||= '[]'
 end
-
-ENV['photo_fetch_timer'] = appSettings.photo_fetch_timer.to_s || '60'
-ENV['photo_switch_timer'] = appSettings.photo_switch_timer.to_s || '30'
-ENV['hashtags'] ||= '[]'
