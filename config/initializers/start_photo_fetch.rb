539a7cf685f843b429672ebf6a5017dbd2afcfb1
Brad Ringel <brad.ringel@gmail.com>
2016-07-27 19:39:33 -0400

    back to the if statements

--- config/initializers/start_photo_fetch.rb
+++ config/initializers/start_photo_fetch.rb
@@ -5,19 +5,20 @@ scheduler = Rufus::Scheduler.singleton
 current_instagram_user = UserAccount.where(:account_type => UserAccount.account_types[:instagram]).first
 current_twitter_user = UserAccount.where(:account_type => UserAccount.account_types[:twitter]).first
 
-exit if !ENV['hashtags'] || !ENV['photo_fetch_timer']
+if ENV['hashtags'] && ENV['photo_fetch_timer']
 
-if current_instagram_user
-  instagram_client = InstagramClient.new(current_instagram_user.auth_token)
-  hashtags = JSON.parse(ENV['hashtags'])
-
-  instagram_client.fetch_all_recent_photos(hashtags)
-
-  scheduler.every("#{ENV['photo_fetch_timer']}s") do
-    current_instagram_user = UserAccount.where(:account_type => UserAccount.account_types[:instagram]).first
+  if current_instagram_user
     instagram_client = InstagramClient.new(current_instagram_user.auth_token)
-    hashtags = JSON.parse(ENV['hashtags']) # get the hashtags again inside the block
+    hashtags = JSON.parse(ENV['hashtags'])
+
     instagram_client.fetch_all_recent_photos(hashtags)
+
+    scheduler.every("#{ENV['photo_fetch_timer']}s") do
+      current_instagram_user = UserAccount.where(:account_type => UserAccount.account_types[:instagram]).first
+      instagram_client = InstagramClient.new(current_instagram_user.auth_token)
+      hashtags = JSON.parse(ENV['hashtags']) # get the hashtags again inside the block
+      instagram_client.fetch_all_recent_photos(hashtags)
+    end
   end
 
   if current_twitter_user
