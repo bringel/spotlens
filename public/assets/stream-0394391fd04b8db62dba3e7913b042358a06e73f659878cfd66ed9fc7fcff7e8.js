ab2cd586dfda9b9fff92e7d488e568ff3cc1a8bf
Brad Ringel <brad.ringel@gmail.com>
2016-08-30 21:59:51 -0400

    tweak the javascript so that the fetch polyfill gets added to the application.js file

--- public/assets/stream-0394391fd04b8db62dba3e7913b042358a06e73f659878cfd66ed9fc7fcff7e8.js
+++ public/assets/stream-0394391fd04b8db62dba3e7913b042358a06e73f659878cfd66ed9fc7fcff7e8.js
@@ -1,49 +1,47 @@
+"use strict";
+
 var currentTwitterPhotoID, currentInstagramPhotoID, moreTwitterPhotos, moreInstagramPhotos, currentTwilioPhotoID;
 
-function flipCoin(){
+function flipCoin() {
   var sources = ["instagram", "twitter", "twilio"];
   var flipped = Math.floor(Math.random() * 3);
 
   return sources[flipped];
 }
 
-function photoTimerFired(){
+function photoTimerFired() {
   var result = flipCoin();
-  if(result === "instagram"){
-    getNextInstagramPhoto(currentInstagramPhotoID).then(function(response){
+  if (result === "instagram") {
+    getNextInstagramPhoto(currentInstagramPhotoID).then(function (response) {
       response = JSON.parse(response);
       parseAndDisplayPhotoData(response, result);
     });
-  }
-  else if(result === "twitter"){
-    getNextTwitterPhoto(currentTwitterPhotoID).then(function(response){
+  } else if (result === "twitter") {
+    getNextTwitterPhoto(currentTwitterPhotoID).then(function (response) {
       response = JSON.parse(response);
-      parseAndDisplayPhotoData(response, result)
-    })
-  }
-  else if(result === "twilio"){
-    getNextTwilioPhoto(currentTwilioPhotoID).then((response) => {
-      if(response.ok){
+      parseAndDisplayPhotoData(response, result);
+    });
+  } else if (result === "twilio") {
+    getNextTwilioPhoto(currentTwilioPhotoID).then(function (response) {
+      if (response.ok) {
         parseAndDisplayPhotoData(response.json(), result);
       }
-    })
+    });
   }
 }
 
-function getNextInstagramPhoto(currentID){
-  var promise = new Promise(function(resolve, reject){
-    let request = new XMLHttpRequest();
-    if(currentID){
-      request.open("get", `${window.location.origin}/stream/next_instagram_photo?current=${currentID}`);
+function getNextInstagramPhoto(currentID) {
+  var promise = new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    if (currentID) {
+      request.open("get", window.location.origin + "/stream/next_instagram_photo?current=" + currentID);
+    } else {
+      request.open("get", window.location.origin + "/stream/next_instagram_photo");
     }
-    else{
-      request.open("get", `${window.location.origin}/stream/next_instagram_photo`);
-    }
-    request.onload = function(){
-      if(request.status >= 200 && request.status < 300){
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 300) {
         resolve(request.response);
-      }
-      else{
+      } else {
         reject(request.status);
       }
     };
@@ -52,20 +50,18 @@ function getNextInstagramPhoto(currentID){
   return promise;
 }
 
-function getNextTwitterPhoto(currentID){
-  var promise = new Promise(function(resolve, reject){
-    let request = new XMLHttpRequest();
-    if(currentID){
-      request.open("get", `${window.location.origin}/stream/next_twitter_photo?current=${currentID}`);
+function getNextTwitterPhoto(currentID) {
+  var promise = new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    if (currentID) {
+      request.open("get", window.location.origin + "/stream/next_twitter_photo?current=" + currentID);
+    } else {
+      request.open("get", window.location.origin + "/stream/next_twitter_photo");
     }
-    else{
-      request.open("get", `${window.location.origin}/stream/next_twitter_photo`);
-    }
-    request.onload = function(){
-      if(request.status >= 200 && request.status < 300){
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 300) {
         resolve(request.response);
-      }
-      else{
+      } else {
         reject(request.status);
       }
     };
@@ -74,16 +70,15 @@ function getNextTwitterPhoto(currentID){
   return promise;
 }
 
-function getNextTwilioPhoto(currentID){
-  if(currentID){
-    return fetch(`${window.location.origin}/stream/next_twilio_photo?current=${currentID}`);
-  }
-  else{
-    return fetch(`${window.location.origin}/stream/next_twilio_photo`);
+function getNextTwilioPhoto(currentID) {
+  if (currentID) {
+    return fetch(window.location.origin + "/stream/next_twilio_photo?current=" + currentID);
+  } else {
+    return fetch(window.location.origin + "/stream/next_twilio_photo");
   }
 }
 
-function loaded(){
+function loaded() {
   // getNextTwitterPhoto().then(function(response){
   //   response = JSON.parse(response);
   //   parseAndDisplayPhotoData(response, "twitter");
@@ -93,35 +88,32 @@ function loaded(){
   photoTimerFired();
 }
 
-function parseAndDisplayPhotoData(result, photoSource){
+function parseAndDisplayPhotoData(result, photoSource) {
   var photoUrl, profilePhotoUrl, username, fullname, caption, likes;
 
-  if(photoSource === "instagram"){
+  if (photoSource === "instagram") {
     currentInstagramPhotoID = result.id;
 
     photoUrl = result.photo_url;
     profilePhotoUrl = result.instagram_profile_photo;
 
-    username = `@${result.instagram_username}`;
+    username = "@" + result.instagram_username;
     fullname = result.instagram_fullname;
 
     caption = result.caption;
     likes = result.likes;
-
-  }
-  else if(photoSource === "twitter"){
+  } else if (photoSource === "twitter") {
     currentTwitterPhotoID = result.id;
 
     photoUrl = result.photo_url;
     profilePhotoUrl = result.twitter_profile_photo;
 
-    username = `@${result.twitter_username}`;
+    username = "@" + result.twitter_username;
     fullname = result.twitter_fullname;
 
     caption = result.tweet_text;
     likes = result.favorites;
-  }
-  else if(photoSource === "twilio"){
+  } else if (photoSource === "twilio") {
     currentTwilioPhotoID = result.id;
 
     photoUrl = result.media_url;
@@ -132,65 +124,58 @@ function parseAndDisplayPhotoData(result, photoSource){
   setTimeout(photoTimerFired, photoSwitchTimer * 1000);
 }
 
-function displayPhoto(photoUrl, profilePhotoUrl, username, fullname, caption, likes){
+function displayPhoto(photoUrl, profilePhotoUrl, username, fullname, caption, likes) {
 
   var imageContainer = document.getElementById("currentPhoto");
-  if(imageContainer.children.length > 0 && imageContainer.firstElementChild.tagName.toLowerCase() === "img"){
+  if (imageContainer.children.length > 0 && imageContainer.firstElementChild.tagName.toLowerCase() === "img") {
     imageContainer.firstElementChild.src = photoUrl;
-  }
-  else{
+  } else {
     var imageTag = new Image();
     imageContainer.appendChild(imageTag);
     imageTag.src = photoUrl;
   }
 
   var profilePhotoContainer = document.getElementById("profile");
-  if(profilePhotoUrl){
+  if (profilePhotoUrl) {
     profilePhotoContainer.style.visibility = '';
-    if(profilePhotoContainer.children.length > 0 && profilePhotoContainer.firstElementChild.tagName.toLowerCase() === "img"){
+    if (profilePhotoContainer.children.length > 0 && profilePhotoContainer.firstElementChild.tagName.toLowerCase() === "img") {
       profilePhotoContainer.firstElementChild.src = profilePhotoUrl;
-    }
-    else{
+    } else {
       var profilePhoto = new Image();
       profilePhotoContainer.appendChild(profilePhoto);
       profilePhoto.src = profilePhotoUrl;
     }
-  }
-  else{
+  } else {
     profilePhotoContainer.style.visibility = 'hidden';
   }
 
   var userNameSpan = document.getElementById("username");
   var fullNameSpan = document.getElementById("fullname");
-  if(username && fullname){
+  if (username && fullname) {
     userNameSpan.style.visibility = '';
     fullNameSpan.style.visibility = '';
     userNameSpan.textContent = username;
     fullNameSpan.textContent = fullname;
-  }
-  else{
+  } else {
     userNameSpan.style.visibility = 'hidden';
     fullNameSpan.style.visibility = 'hidden';
   }
 
   var captionSpan = document.getElementById("captionText");
-  if(caption){
+  if (caption) {
     captionSpan.style.visibility = '';
     captionSpan.textContent = caption;
-  }
-  else{
+  } else {
     captionSpan.style.visibility = 'hidden';
   }
 
   var heartsCount = document.getElementById("likeCount");
   var heart = document.getElementById("heartIcon");
-  if(likes){
+  if (likes) {
     heartsCount.style.visibility = '';
     heart.style.visibility = '';
     heartsCount.textContent = likes;
-
-  }
-  else{
+  } else {
     heartsCount.style.visibility = 'hidden';
     heart.style.visibility = 'hidden';
   }
